edb70757255ddcc9283b0ded5f1fe68c2dac814f
pvcong <pvcong@gmail.com>
2020-03-22 20:44:25 +0700

    Add stopwatch for no-ping file

--- my-app/src/app/file-upload/file-upload.component.ts
+++ my-app/src/app/file-upload/file-upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Subscription, timer } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-file-upload',
@@ -7,18 +8,27 @@ import { Subscription, timer } from 'rxjs';
   styleUrls: [ './file-upload.component.scss' ]
 })
 export class FileUploadComponent implements OnInit {
+  // for timer
+  counter: number;
+  timerRef = 0;
+  running = false;
+  // end for timer
   pingFileSubscription: Subscription;
   noPingFiles: FileList;
   withPingFiles: FileList;
 
   @ViewChild('eventLog', { static: false })
   eventLog?: ElementRef<HTMLElement>;
+
   constructor() {}
 
   ngOnInit() {}
 
   appendMsgToEventLog(msg: string) {
-    this.eventLog.nativeElement.textContent = this.eventLog.nativeElement.textContent + '\n' + msg;
+    const eventLogEl = this.eventLog.nativeElement;
+    const now = moment().format('LTS');
+    eventLogEl.textContent = this.eventLog.nativeElement.textContent + '\n' + now + ' ' + msg;
+    eventLogEl.scrollTop = eventLogEl.scrollHeight;
   }
 
   onSelectedFiles(e, ping: boolean) {
@@ -34,6 +44,9 @@ export class FileUploadComponent implements OnInit {
         this.pingSelectedFiles(files);
       }
     } else {
+      this.clearTimer();
+      this.counter = 0;
+      this.startTimer();
       this.noPingFiles = files;
     }
   }
@@ -53,7 +66,7 @@ export class FileUploadComponent implements OnInit {
   }
 
   pingFile(file: File) {
-    this.appendMsgToEventLog('start to ping selected file: ' + file.name);
+    this.appendMsgToEventLog('start to read some first bytes of selected file: ' + file.name);
     const reader = new FileReader();
     reader.onabort = () => {
       this.appendMsgToEventLog('reader onabort');
@@ -71,22 +84,11 @@ export class FileUploadComponent implements OnInit {
     reader.readAsText(blob);
   }
 
-  onStartTimer() {
+  uploadSelectedFiles() {
     if (this.pingFileSubscription) {
       this.pingFileSubscription.unsubscribe();
+      this.appendMsgToEventLog('stopped ping files');
     }
-    const checkingTimer = timer(100, 2000);
-    this.pingFileSubscription = checkingTimer.subscribe((val) => {
-      console.log(val);
-    });
-  }
-  onStopTimer() {
-    this.pingFileSubscription.unsubscribe();
-    this.appendMsgToEventLog('stopped ping files');
-  }
-  uploadSelectedFiles() {
-    this.pingFileSubscription.unsubscribe();
-    this.appendMsgToEventLog('stopped ping files');
     // now, we try to read the content of selected files
     // first read with-ping files
     this.appendMsgToEventLog('try to read with-ping files');
@@ -94,11 +96,29 @@ export class FileUploadComponent implements OnInit {
       this.pingFiles(this.withPingFiles);
     }
     // second if no-ping files, we wait 3 seconds to ensure with-ping file is readed
-    this.appendMsgToEventLog('try to read no-ping files');
     setTimeout(() => {
+      this.appendMsgToEventLog('try to read no-ping files');
       if (this.noPingFiles && this.noPingFiles.length > 0) {
         this.pingFiles(this.noPingFiles);
       }
-    }, 2000);
+    }, 3000);
+  }
+
+  startTimer() {
+    this.running = !this.running;
+    if (this.running) {
+      const startTime = Date.now() - (this.counter || 0);
+      this.timerRef = setInterval(() => {
+        this.counter = Math.round((Date.now() - startTime) / 1000);
+      });
+    } else {
+      clearInterval(this.timerRef);
+    }
+  }
+
+  clearTimer() {
+    this.running = false;
+    this.counter = 0;
+    clearInterval(this.timerRef);
   }
 }
